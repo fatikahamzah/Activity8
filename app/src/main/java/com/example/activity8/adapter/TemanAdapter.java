7ae8e395d00123cbf409b07f319e2c4d7ebcb97c
fatikahamzah <fatikahmzptr@gmail.com>
2022-05-23 23:09:19 +0700

    Activity10_PAM

--- app/src/main/java/com/example/activity8/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/activity8/adapter/TemanAdapter.java
@@ -117,7 +117,7 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
     }
 
     private void Hapusdata(final String idx){
-        String url_update = "http://10.0.2.2/PAM/deletetm.php";
+        String url_update = "https://20200140038.praktikumtiumy.com/deletetm.php";
         final String TAG = MainActivity.class.getSimpleName();
         final String TAG_SUCCESS = "success";
         final int[] sukses = new int[1];
